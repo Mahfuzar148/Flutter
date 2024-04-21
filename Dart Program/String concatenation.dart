2ff200938843060ee20a6381d22ed9f7c562eba7
Mahfuzar Rahman <113121842+Mahfuzar148@users.noreply.github.com>
2024-04-21 10:49:05 +0600

    Create String concatenation.dart

---
+++ Dart Program/String concatenation.dart
@@ -0,0 +1,14 @@
+void main() {
+  String a = 'We';
+  String b = 'are';
+  String c = 'learning';
+  String d = 'dart';
+//uing + sign
+  print(a + ' ' + b + ' ' + c + ' ' + d);
+  print('$a $b $c $d');
+  List<String> f = [a, b, c, d];
+
+  print(f.join(' '));
+  String e = 'We' 'are' 'learing' 'dart';
+  print(e);
+}
